9d2051264980d60cac37e91e9c03ea379cbd9e41
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2007-08-26 17:30:42 +0000

    libs update

--- Mailster.bat
+++ Mailster.bat
@@ -4,7 +4,7 @@
 SET JAVA_OPTIONS=-server
 
 SET CP="mailster.jar;lib\swt-win.jar;lib\mailapi.jar;lib\jface.jar;lib\glazedlists-1.7.0_java15.jar;%CLASSPATH%"
-SET CP="lib\bcprov-jdk15-136.jar;lib\mina-core-1.1.0.jar;lib\mina-filter-ssl-1.1.0.jar;lib\slf4j-api-1.3.1.jar;%CP%"
-SET CP="lib\slf4j-simple-1.3.1.jar;%CP%"
+SET CP="lib\bcprov-jdk15-137.jar;lib\mina-core-1.1.2.jar;lib\mina-filter-ssl-1.1.2.jar;lib\slf4j-api-1.4.3.jar;%CP%"
+SET CP="lib\slf4j-simple-1.4.3.jar;%CP%"
 
 java %JAVA_OPTIONS% -cp %CP% org.mailster.MailsterSWT 300
