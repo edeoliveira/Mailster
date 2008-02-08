905eab00f1a41c35ea46b899658eb092e755a157
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2008-02-08 15:44:21 +0000

    File manipulation utilities

---
+++ org/mailster/util/FileUtilities.java
@@ -0,0 +1,50 @@
+package org.mailster.util;
+
+import java.io.File;
+
+/**
+ * ---<br>
+ * Mailster (C) 2007 De Oliveira Edouard
+ * <p>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+ * Ave, Cambridge, MA 02139, USA.
+ * <p>
+ * See&nbsp; <a href="http://mailster.sourceforge.net" target="_parent">Mailster
+ * Web Site</a> <br>
+ * ---
+ * <p>
+ * FileUtilities.java - Various methods to help in file handling.
+ * 
+ * @author <a href="mailto:doe_wanted@yahoo.fr">Edouard De Oliveira</a>
+ * @version $Revision$, $Date$
+ */
+public class FileUtilities 
+{
+    public static boolean deleteDirectory(File dir) 
+    {
+        if (dir.isDirectory()) 
+        {
+            String[] children = dir.list();
+            boolean success = true;
+            for (int i=0; i<children.length; i++) 
+                success = success && deleteDirectory(new File(dir, children[i]));
+
+            if (!success)
+                return false;
+        }
+    
+        // The directory is now empty so delete it
+        return dir.delete();
+    }
+}
