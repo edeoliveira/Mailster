510e5eb20b562869f552677e5b360cc802eb3f65
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2008-06-10 12:46:49 +0000

    Ensure assertions are disabled in GL.

--- mailster.sh
+++ mailster.sh
@@ -2,7 +2,7 @@
 
 #Debug options
 #export JAVA_OPTIONS=-Xdebug -Xnoagent -Xrunjdwp:transport=dt_socket,address=999,server=y,suspend=n -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=4321 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
-export JAVA_OPTIONS=-server -Xms256m -Xmx256m
+export JAVA_OPTIONS=-server -Xms256m -Xmx256m -da:ca.odell.glazedlists...
 
 export JAVA_HOME=/usr/java/jre1.6.0_01/
 export PATH=$PATH:$JAVA_HOME/bin
