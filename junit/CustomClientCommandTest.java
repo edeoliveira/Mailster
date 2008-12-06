8441f61a31e1113003e64a07d6bcb4fe592455ef
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2008-12-06 00:04:20 +0000

    Updated headers

--- junit/CustomClientCommandTest.java
+++ junit/CustomClientCommandTest.java
@@ -11,6 +11,33 @@ import java.util.Iterator;
 import org.mailster.smtp.SimpleSmtpServer;
 import org.mailster.smtp.SmtpMessage;
 
+/**
+ * ---<br>
+ * Mailster (C) 2007-2009 De Oliveira Edouard
+ * <p>
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 2 of the License, or (at your option) any later
+ * version.
+ * <p>
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * <p>
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 675 Mass
+ * Ave, Cambridge, MA 02139, USA.
+ * <p>
+ * See&nbsp; <a href="http://mailster.sourceforge.net" target="_parent">Mailster
+ * Web Site</a> <br>
+ * ---
+ * <p>
+ * CustomClientCommandTest.java - Minimal client that tests smtp responses.
+ * 
+ * @author <a href="mailto:doe_wanted@yahoo.fr">Edouard De Oliveira</a>
+ * @version $Revision$, $Date$
+ */
 public class CustomClientCommandTest extends TestCase 
 {
 
