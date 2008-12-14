07afd235ec25389d20216065acad75791863fe71
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2008-12-14 00:28:38 +0000

    Upgraded libs versions

--- mailster.sh
+++ mailster.sh
@@ -8,9 +8,9 @@ export JAVA_HOME=/usr/java/jre1.6.0_01/
 export PATH=$PATH:$JAVA_HOME/bin
 
 export CP=./mailster.jar:lib/swt-gtklinux.jar:lib/mailapi.jar:lib/glazedlists_java15.jar:$CLASSPATH
-export CP=lib/bcprov-jdk15-137.jar:lib/bcmail-jdk15-137.jar:$CP
-export CP=lib/mina-core-1.1.6.jar:lib/mina-filter-ssl-1.1.6.jar:$CP
-export CP=lib/slf4j-simple-1.4.3.jar:lib/slf4j-api-1.4.3.jar:$CP
+export CP=lib/bcprov-jdk15-141.jar:lib/bcmail-jdk15-141.jar:$CP
+export CP=lib/mina-core-1.1.7.jar:lib/mina-filter-ssl-1.1.7.jar:$CP
+export CP=lib/slf4j-simple-1.5.6.jar:lib/slf4j-api-1.5.6.jar:$CP
 export CP=lib/org.eclipse.core.commands_3.3.0.I20070605-0010.jar:lib/org.eclipse.equinox.common_3.3.0.v20070426.jar;$CP
 export CP=lib/org.eclipse.jface_3.3.0.I20070606-0010.jar;$CP
 
