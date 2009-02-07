1bf4e5157d417368b547778339238234af67cd5b
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2009-02-07 22:46:11 +0000

    Updated scripts lib

--- Mailster.bat
+++ Mailster.bat
@@ -5,13 +5,20 @@ SET JAVA_OPTIONS=-Xms256m -Xmx256m -da:ca.odell.glazedlists...
 
 SET JAVA_HOME=C:\Program Files\Java\jdk1.6.0_01
 
-SET CP=mailster.jar;lib\swt.jar;lib\swt-win.jar;lib\mailapi.jar;lib\glazedlists_java15.jar;%CLASSPATH%
+SET CP=mailster.jar;lib\mail.jar;lib\glazedlists_java15.jar;%CLASSPATH%
 SET CP=lib\bcprov-jdk15-141.jar;lib\bcmail-jdk15-141.jar;%CP%
-SET CP=lib\mina-core-1.1.7.jar;lib\mina-filter-ssl-1.1.7.jar;%CP%
-SET CP=lib\slf4j-api-1.5.6.jar;lib\slf4j-simple-1.5.6.jar;%CP%
-set CP=lib\org.eclipse.core.commands_3.3.0.I20070605-0010.jar;lib\org.eclipse.equinox.common_3.3.0.v20070426.jar;%CP%
-set CP=lib\org.eclipse.jface_3.3.0.I20070606-0010.jar;%CP%
+SET CP=lib\mina-core-2.0.0-M4.jar;lib\MailsterSMTP.1.0.0-M2.jar;%CP%
+SET CP=lib\slf4j-api-1.5.6.jar;lib\slf4j-log4j12-1.5.6.jar;lib\log4j-1.2.15.jar;%CP%
 set CP=lib\commons-lang-2.3.jar;%CP%
+
+set CP=lib\org.eclipse.jface_3.4.1.M20080827-2000.jar;%CP%
+set CP=lib\org.eclipse.swt_3.4.1.v3449c.jar;%CP%
+set CP=lib\org.eclipse.core.runtime_3.4.0.v20080512.jar;%CP%
+set CP=lib\org.eclipse.core.commands_3.4.0.I20080509-2000.jar;%CP%
+set CP=lib\org.eclipse.equinox.common_3.4.0.v20080421-2006.jar;%CP%
+set CP=lib\org.eclipse.osgi_3.4.2.R34x_v20080826-1230.jar;%CP%
+set CP=lib\org.eclipse.swt.win32.win32.x86_3.4.1.v3449c.jar;%CP%
+
 SET CP="%CP%"
 
 java %JAVA_OPTIONS% -cp %CP% org.mailster.MailsterSWT
