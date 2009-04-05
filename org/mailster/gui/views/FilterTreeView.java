7bb7d8d4def6625c68ebd7bdb1ca4211df5234ad
Edouard DE OLIVEIRA <doe_wanted@yahoo.fr>
2009-04-05 12:06:05 +0000

    Added drag&drop

--- org/mailster/gui/views/FilterTreeView.java
+++ org/mailster/gui/views/FilterTreeView.java
@@ -1,29 +1,17 @@
 package org.mailster.gui.views;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 
 import javax.mail.Flags;
 
-import org.apache.mina.filter.codec.textline.LineDelimiter;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Dialog;
-import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.swt.widgets.Display;
-import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
@@ -33,18 +21,11 @@ import org.mailster.MailsterSWT;
 import org.mailster.gui.DropDownListener;
 import org.mailster.gui.Messages;
 import org.mailster.gui.SWTHelper;
-import org.mailster.gui.dialogs.ErrorDialog;
-import org.mailster.message.SmtpMessage;
-import org.mailster.message.SmtpMessageFactory;
 import org.mailster.pop3.mailbox.StoredSmtpMessage;
-import org.mailster.server.MailsterConstants;
-import org.mailster.server.MailsterSmtpService;
-import org.mailster.util.StreamUtilities;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import ca.odell.glazedlists.AbstractEventList;
-import ca.odell.glazedlists.EventList;
 import ca.odell.glazedlists.FilterList;
 import ca.odell.glazedlists.FunctionList;
 import ca.odell.glazedlists.GlazedLists;
@@ -257,177 +238,18 @@ public class FilterTreeView extends TreeView
         	menu.setDefaultItem(item);
 
 			if (item == importAsMailItem)
-				importFromEmailFile();
+				ImportExportUtilities.importFromEmailFile();
 			else
 			if (item == importAsMailBoxItem)
-				importFromMbox();
+				ImportExportUtilities.importFromMbox();
 			else
 			if (item == exportAsMailItem)
-				exportAsEmailFile();
+				ImportExportUtilities.exportAsEmailFile();
 			else
 			if (item == exportAsMailBoxItem)
-				exportAsMbox();
+				ImportExportUtilities.exportAsMbox();
         }
         
-        private EventList<StoredSmtpMessage> getEmailSelection()
-        {
-    		EventList<StoredSmtpMessage> mails = 
-    			MailsterSWT.getInstance().getMailView().getTableView().getSelection();
-    		
-    		if (mails == null || mails.size() == 0)
-    			mails = MailsterSWT.getInstance().getMailView().getTableView().getTableList();
-
-    		return mails;
-        }
-
-        private void importFromEmailFile()
-        {
-			String fileName = getPath(true, false, false, SWT.OPEN);
-        	
-        	if (fileName == null)
-        		return;
-        	
-        	try
-            {        		
-        		FileInputStream in = new FileInputStream(fileName);
-                
-        		SmtpMessageFactory factory = 
-        			new SmtpMessageFactory(MailsterConstants.DEFAULT_CHARSET, 
-        					new LineDelimiter("\n"));
-                
-        		MailsterSmtpService smtp = MailsterSWT.getInstance().getSMTPService();
-        		smtp.addReceivedEmail(factory.asSmtpMessage(in, null));
-        		smtp.refreshEmailQueue(false);
-                in.close();
-            }
-            catch (Exception e)
-            {
-            	ErrorDialog dlg = new ErrorDialog(MailsterSWT.getInstance().getShell(), 
-            			"Exception occured", "Failed importing email file : "+fileName, new Status(IStatus.ERROR, "Mailster", "Unable to import file", e), IStatus.ERROR);
-            	dlg.open();
-            }
-        }
-        
-        private void importFromMbox()
-        {
-			String fileName = getPath(true, true, false, SWT.OPEN);
-        	
-        	if (fileName == null)
-        		return;
-        	
-        	try
-            {        		
-        		BufferedReader in = new BufferedReader(new FileReader(fileName));
-                
-                List<SmtpMessage> mails = StreamUtilities.
-                	readMessageFromMBoxRDFormat(in, MailsterConstants.DEFAULT_CHARSET);
-                
-                MailsterSmtpService smtp = MailsterSWT.getInstance().getSMTPService();
-                smtp.addReceivedEmail(mails);
-                smtp.refreshEmailQueue(false);
-                in.close();
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-        
-        private void exportAsEmailFile()
-        {
-        	EventList<StoredSmtpMessage> mails = getEmailSelection();
-        	String path = getPath(false, false, true,SWT.SAVE);
-        	
-        	if (path == null)
-        		return;
-        	
-    		for (StoredSmtpMessage msg : mails)
-    		{
-    			PrintWriter out = null;
-    			
-    			try
-                {
-	    				out = new PrintWriter(new FileWriter(path+File.separatorChar+
-	    						msg.getMessageId().substring(1,msg.getMessageId().length()-1)+".eml", false));
-    				out.write(msg.getMessage().toString());                
-                }
-                catch (Exception e)
-                {
-                    e.printStackTrace();
-                }        	
-                finally
-                {
-                	if (out != null)
-                		out.close();
-                }
-    		}
-        }
-        
-        private void exportAsMbox()
-        {
-        	EventList<StoredSmtpMessage> mails = getEmailSelection();
-			String fileName = getPath(false, true, false, SWT.SAVE);
-        	
-        	if (fileName == null)
-        		return;
-        	
-        	try
-            {        		
-                PrintWriter out = new PrintWriter(new FileWriter(fileName, false));
-                for (StoredSmtpMessage msg : mails)
-                	StreamUtilities.writeMessageToMBoxRDFormat(msg, out);
-                
-                out.close();
-            }
-            catch (Exception e)
-            {
-                e.printStackTrace();
-            }
-        }
-        
-        private String getPath(boolean importMode, boolean mboxMode, boolean isDirectoryMode, int dialogMode)
-        {
-        	Dialog d =  null;
-        	
-        	if (isDirectoryMode)
-        		d = new DirectoryDialog(dropdown.getParent().getShell(), dialogMode);
-        	else
-        		d = new FileDialog(dropdown.getParent().getShell(), dialogMode);
-
-	        if (importMode)
-	        	d.setText(Messages.getString("FilterTreeview.import.dialog.title")); //$NON-NLS-1$
-	        else
-	        	d.setText(Messages.getString("FilterTreeview.export.dialog.title")); //$NON-NLS-1$
-	                	
-        	if (isDirectoryMode)
-        		return ((DirectoryDialog) d).open();
-        	else
-        	{
-        		FileDialog dialog = (FileDialog) d;
-        		
-		        if (mboxMode)
-		        {
-			        dialog.setFilterNames(new String[] {
-			                Messages.getString("FilterTreeview.mbox.files.ext"), //$NON-NLS-1$
-			                Messages.getString("MailView.all.files.ext") }); //$NON-NLS-1$
-			        
-			        dialog.setFilterExtensions(new String[] { "*.mbx", "*.*" }); //$NON-NLS-1$ //$NON-NLS-2$
-		        }
-			    else
-			    {
-			        dialog.setFilterNames(new String[] {
-			                Messages.getString("FilterTreeview.mail.files.ext"), //$NON-NLS-1$
-			                Messages.getString("MailView.all.files.ext") }); //$NON-NLS-1$
-		        	
-			        dialog.setFilterExtensions(new String[] { "*.eml", "*.*" }); //$NON-NLS-1$ //$NON-NLS-2$
-			    }
-		        
-		        dialog.setFilterPath(MailsterSWT.getInstance().getSMTPService().getOutputDirectory());
-		        
-		        return dialog.open();
-        	}
-        }
-
         public void buttonPushed()
         {
         	doMenuAction(menu.getDefaultItem());
@@ -461,6 +283,7 @@ public class FilterTreeView extends TreeView
         
         root.setExpanded(true);
         tree.setSelection(root);
+        MailsterSWT.getInstance().configureDragAndDrop(tree);
     }
     
     protected  void customizeToolbar(ToolBar toolBar)
